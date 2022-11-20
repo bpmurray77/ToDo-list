f881ec1e9ce9923b205b384bcc355c003b6cb0df
bpmurray77 <bpmurray77@gmail.com>
2022-11-20 18:57:06 +0000

    working on local storage method

--- src/js/ToDoForm.js
+++ src/js/ToDoForm.js
@@ -6,26 +6,40 @@ import data from "./data.json";
 
 
 function ToDoForm() {
-    const [ToDo, setToDo] = useState({
-        id:"",
-        task:"",
-        completed: false
-    });
 
-    function handleInput(e){
-        setToDo({...ToDo, task: e.target.value})
-    }
+    let allTasks = []
+
+    const addToDo = (ev) => {
+        ev.preventDefault();
+        let task = {
+            task: document.getElementById("input").value,
+            complete: false,
+        };
+        allTasks.push(task);
+
+        console.log(allTasks);
+
+        localStorage.setItem("Inventory", JSON.stringify(allTasks));
+      };
+    document.addEventListener("DOMContentLoaded", () => {
+        document.getElementById("button").addEventListener("click", addToDo)});
+    document.addEventListener("DOMContentLoaded", () => {
+        document.getElementById("button").addEventListener("click", document.forms[0].reset())
+      });
+
+
 
     return (
         <form>
             <input
             name="task"
+            id="input"
             type="text"
-            value={ToDo.task}
-            onChange={handleInput}
+            onChange={addToDo}
             ></input>
             <button
-            type="submit"></button>
+            type="submit"
+            id="button"></button>
         </form>
     );
  };
